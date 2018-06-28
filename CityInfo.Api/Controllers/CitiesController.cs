9684198e2a767220e488b1cd39acb8bea2bbab98
Kevin Gagnon <klg73@outlook.com>
2018-06-28 14:27:51 -0400

    Cities call

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cities")]
+    public class CitiesController : Controller
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new {id=1, Name="New York"},
+                new {id=1, Name="Antwerp"}
+            });
+        }
+    }
+}
