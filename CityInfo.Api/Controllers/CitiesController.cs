77455abee671d92cce7ac6d08a0e7f4625120850
Kevin Gagnon <klg73@outlook.com>
2018-06-29 07:17:54 -0400

    Cities Data Store added

--- CityInfo.Api/Controllers/CitiesController.cs
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -8,16 +8,21 @@ using Microsoft.AspNetCore.Mvc;
 namespace CityInfo.Api.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Cities")]
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
+        [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>
-            {
-                new {id=1, Name="New York"},
-                new {id=1, Name="Antwerp"}
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+                );
         }
     }
 }
