14300ffa017f2bd8294bee31ae0a40618fff32e6
Kevin Gagnon <klg73@outlook.com>
2018-07-02 18:15:04 -0400

    Updates that were not synced

--- CityInfo.Api/Controllers/PointsOfInterestController.cs
+++ CityInfo.Api/Controllers/PointsOfInterestController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,7 +24,7 @@ namespace CityInfo.Api.Controllers
             return Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -39,6 +40,80 @@ namespace CityInfo.Api.Controllers
             }
 
             return Ok(pointOfInterest);
+        }
+
+        [HttpPut()]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterestCreationDto)
+        {
+            if (pointOfInterestCreationDto == null)
+            {
+                BadRequest();
+            }
+
+            if (pointOfInterestCreationDto.Name == pointOfInterestCreationDto.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); //This will return the error message from the DTO.
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterestCreationDto.Name,
+                Description = pointOfInterestCreationDto.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, id = finalPointOfInterest.Id }, finalPointOfInterest);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id,[FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different from the name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); //This will return the error message from the DTO.
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var poiFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+            if (poiFromStore == null)
+            {
+                return NotFound();
+            }
+
+            poiFromStore.Name = pointOfInterest.Name;
+            poiFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
         }
     }
 }
--- CityInfo.Api/Models/PointOfInterestForUpdateDto.cs
+++ CityInfo.Api/Models/PointOfInterestForUpdateDto.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace CityInfo.Api.Models
 {
-    public class PointOfInterestForCreationDto
+    public class PointOfInterestForUpdateDto
     {
+        [Required]
+        public int Id { get; set; }
         [Required(ErrorMessage = "Name is a required field.")] //This puts the error back in the model state send
         [MaxLength(50)]
         public string Name { get; set; }
